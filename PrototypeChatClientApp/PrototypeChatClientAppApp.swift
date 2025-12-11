29f7961c45b427beac5193d2e9bca95eaad4f3dd
linnefromice <linnefromice@gmail.com>
2025-12-11 19:59:23 +0900

    Add initial iOS chat client app with Xcode project setup

---
+++ PrototypeChatClientApp/PrototypeChatClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeChatClientAppApp.swift
+//  PrototypeChatClientApp
+//
+//  Created by arata.haruyama on 2025/12/11.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeChatClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrototypeChatClientAppTests/PrototypeChatClientAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeChatClientAppTests.swift
+//  PrototypeChatClientAppTests
+//
+//  Created by arata.haruyama on 2025/12/11.
+//
+
+import Testing
+@testable import PrototypeChatClientApp
+
+struct PrototypeChatClientAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
